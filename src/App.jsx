f3a54f80f180f3b49484776318dcecdb8511ca65
vakhil08 <akhilv1508@gmail.com>
2025-02-19 15:55:59 +0530

    third
    thid

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Login from './Login'
 import Home from './Home'
+import Insta from './Insta'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/insta" element={<Insta/>}/>
      </Routes>
     </>
   )
